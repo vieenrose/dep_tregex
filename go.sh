bfc6f457ab7eefad8bc0cc1f6573c50ba7155ad0
vieenrose <luigi.liu@laposte.net>
2019-03-31 22:13:48 +0200

    return the focalized part of cleft constructions instead of the main copula

--- go.sh
+++ go.sh
@@ -1,27 +1,34 @@
 set -vx
 
+## identify the focalized part of cleft constructions in Naija
+## using tregex
+## ref: https://yandex.github.io/dep_tregex/index.html
 
-# "A .<-- B" : A have a child (B) in the left
-# "A <--. B" : A have a head (B) in the left
-# "A .--> B" : A have a head (B) in the right
-# "A -->. B" : A have a child (B) in the right
+## 
+# "A .<-- B" : A has a child (B) to the left
+# "A <--. B" : A has a head (B) to the right
+# "A .--> B" : A has a head (B) to the left
+# "A -->. B" : A has a child (B) to the right
+
+COPULA='x form /na/ and cpostag /PART/'
+COPULA2='y form /na/ and cpostag /PART/ and deprel /comp:cleft/'
+FOCALIZED='f deprel /comp:pred/'
+FOCALIZED2='f2 deprel /comp:pred/ and form /im/'
+REST='t deprel /comp:cleft/'
+RELATIVE='w form /wey/ and cpostag /SCONJ/ and deprel /dep/'
+DISLOCATED='d deprel /dislocated/'
 
 # covert cleft: na PIDGIN we don grow wit
-covert_cleft='x form /na/ and cpostag /PART/ and -->. t deprel /comp:cleft/ and -->. f deprel /comp:pred/'
-
+covert_cleft="$FOCALIZED and .--> ($COPULA and -->. $REST)"
 # overt cleft: na PIDGIN wey we don grow wit
-overt_cleft='x form /na/ and cpostag /PART/ and -->. f deprel /comp:pred/ and -->. w form /wey/ and cpostag /SCONJ/ and deprel /dep/'
-
+overt_cleft="$FOCALIZED and -->. $RELATIVE"
 # double cleft: na PIDGIN na im we don grow wit
-double_cleft='x form /na/ and cpostag /PART/ and -->. y form /na/ and deprel /comp:cleft/ and cpostag /PART/ and -->. f2 form /im/ and deprel /comp:pred/ and cpostag /PRON/'
-
+double_cleft="$FOCALIZED and .--> ($COPULA and -->. ($COPULA2 and -->. $FOCALIZED2))"
 # pseudo-cleft: wetin we don grow wit na Pidgin
-pseudo_cleft='x form /na/ and cpostag /PART/ and -->. f deprel /comp:pred/ and .<-- t deprel /dislocated/ and form /wetin/'
-
+pseudo_cleft="$FOCALIZED and .--> ($COPULA and .<-- $DISLOCATED and form /wetin/)"
 # revserse pseudo-cleft: Pidgin na wetin we don grow wit
-reverse_pseudo_cleft='x form /na/ and cpostag /PART/ and -->. f deprel /comp:pred/ and form /wetin/ and .<-- t deprel /dislocated/'
-
+reverse_pseudo_cleft="$FOCALIZED and form /wetin/ and .--> ($COPULA and .<-- $DISLOCATED)"
 
 pattern=$double_cleft
 file='test2'
-python -m 'dep_tregex' grep "$pattern" < $file --html
+python -m 'dep_tregex' grep "$pattern" < $file --html --cpostag
